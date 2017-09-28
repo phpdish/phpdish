4690c762ada5a87fef15b9f50389641188684d18
slince <taosikai@yeah.net>
2017-09-28 16:42:33 +0800

    media

---
+++ src/PHPDish/Component/Media/Manager/FileFactoryInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: taosikai
+ * Date: 2017/9/28
+ * Time: 16:24
+ */
+
+namespace PHPDish\Component\Media\Manager;
+
+
+class FileFactoryInterface
+{
+
+}
