9b41d50f6b2690a58db9a96d58a8e07073dad64d
slince <taosikai@yeah.net>
2017-10-25 23:10:24 +0800

    wanshan anzhuang

--- src/PHPDish/Bundle/CoreBundle/Command/InstallDatabaseCommand.php
+++ src/PHPDish/Bundle/CoreBundle/Command/InstallDatabaseCommand.php
@@ -2,11 +2,14 @@
 
 namespace PHPDish\Bundle\CoreBundle\Command;
 
+use Doctrine\DBAL\Connection;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
 class InstallDatabaseCommand extends AbstractInstallCommand
@@ -16,9 +19,14 @@ class InstallDatabaseCommand extends AbstractInstallCommand
      * @var array
      */
     protected $commands = [
-        'doctrine:database:create',
-        'doctrine:migrations:migrate' => ['--no-interaction' => true],
-        'cache:clear',
+        1 => [
+            'command' => 'doctrine:database:create',
+            'options' => [
+                ' --if-not-exists' => true
+            ]
+        ],
+        3 => 'doctrine:migrations:migrate',
+        5 => 'cache:clear',
     ];
     /**
      * {@inheritdoc}
@@ -31,8 +39,7 @@ class InstallDatabaseCommand extends AbstractInstallCommand
             ->setHelp(<<<EOT
 The <info>%command.name%</info> command creates PHPDish database.
 EOT
-            )
-        ;
+            );
     }
 
     /**
@@ -43,13 +50,82 @@ EOT
         $outputStyle = new SymfonyStyle($input, $output);
         $outputStyle->writeln('Creating PHPDish database.');
 
-        $this->executeCommand('doctrine:database:create', $output)
-//            ->executeCommand('doctrine:schema:update', $output, [
-//                '--force' => true
-//            ])
-            ->executeCommand('cache:clear');
+        //如果数据表存在，则询问是否重置
+        if ($this->isDatabasePresent() && $this->isSchemaPresent()) {
+            $outputStyle->writeln('It appears that your database already exists.');
+            $outputStyle->writeln('<error>Warning! This action will erase your database.</error>');
+            $questionHelper = $this->getHelper('question');
+            $question = new ConfirmationQuestion('Would you like to reset it? (y/N) ', false);
 
+            if ($questionHelper->ask($input, $output, $question)) {
+                $this->commands[2] = [
+                    'command' => 'doctrine:schema:drop',
+                    'options' => [
+                        '--force' => true,
+                        '--full-database' => true,
+                    ]
+                ];
+            }
+        }
+
+        //执行创建命令
+        $this->createSchemaWithProgressbar($output);
         $outputStyle->newLine();
     }
 
+    /**
+     * 执行创建命令
+     * @param OutputInterface $output
+     */
+    protected function createSchemaWithProgressbar(OutputInterface $output)
+    {
+        $progressbar = new ProgressBar($output,count($this->commands));
+        foreach ($this->commands as $commandItem) {
+            if (is_string($commandItem)) {
+                $command = $commandItem;
+                $options = [];
+            } else {
+                $command = $commandItem['command'];
+                $options = $commandItem['options'];
+            }
+            $this->executeCommand($command, $output, $options);
+            $progressbar->advance(1);
+        }
+        $progressbar->finish();
+    }
+
+    /**
+     * 检查是否已经创建过数据库
+     * @throws \Exception
+     * @return bool
+     */
+    protected function isDatabasePresent()
+    {
+        $connection = $this->getContainer()->get('doctrine.dbal.default_connection');
+        $databaseName = $connection->getDatabase();
+
+        try {
+            return in_array($databaseName, $connection->getSchemaManager()->listDatabases());
+        } catch (\Exception $exception) {
+            $message = $exception->getMessage();
+            $mysqlDatabaseError = false !== strpos($message, sprintf("Unknown database '%s'", $databaseName));
+            $postgresDatabaseError = false !== strpos($message, sprintf('database "%s" does not exist', $databaseName));
+
+            if ($mysqlDatabaseError || $postgresDatabaseError) {
+                return false;
+            }
+
+            throw $exception;
+        }
+    }
+
+    /**
+     * 检查是否已经创建过表
+     * @return bool
+     */
+    protected function isSchemaPresent()
+    {
+        $connection = $this->getContainer()->get('doctrine.dbal.default_connection');
+        return count($connection->getSchemaManager()->listTables()) > 0;
+    }
 }
