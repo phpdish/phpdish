a1b143de7334db68989da34eb62496d14e00f51f
slince <taosikai@yeah.net>
2017-11-03 16:07:00 +0800

    提取网站描述到paramter

---
+++ src/PHPDish/Component/Core/Twig/ContainerExtension.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace PHPDish\Component\Core\Twig;
+
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class ContainerExtension extends \Twig_Extension implements ContainerAwareInterface
+{
+    use ContainerAwareTrait;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFunctions()
+    {
+        return [
+            new \Twig_SimpleFunction('read_config_parameter', [$this, 'getConfigurationParameter'])
+        ];
+    }
+
+    /**
+     * 获取配置参数
+     * @param string $key
+     * @return mixed
+     */
+    public function getConfigurationParameter($key)
+    {
+        return $this->container->getParameter($key);
+    }
+}
