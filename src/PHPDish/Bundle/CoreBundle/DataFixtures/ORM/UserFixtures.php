c2fee03921d2bb0f754813ce9e46cd45bd17239f
slince <taosikai@yeah.net>
2017-11-10 22:25:10 +0800

    yi

--- src/PHPDish/Bundle/CoreBundle/DataFixtures/ORM/UserFixtures.php
+++ src/PHPDish/Bundle/CoreBundle/DataFixtures/ORM/UserFixtures.php
@@ -3,7 +3,9 @@
 namespace PHPDish\Bundle\CoreBundle\DataFixtures\ORM;
 
 use Doctrine\Common\Persistence\ObjectManager;
+use FOS\UserBundle\Model\UserManagerInterface;
 use FOS\UserBundle\Util\UserManipulator;
+use PHPDish\Component\Core\AvatarGenerator\AvatarGeneratorInterface;
 
 class UserFixtures extends AbstractFixtures
 {
@@ -13,8 +15,24 @@ class UserFixtures extends AbstractFixtures
     public function load(ObjectManager $manager)
     {
         $user1 = $this->getUserManipulator()->create('优雅的风', '123456', 'user1@phpdish.com', true, false);
-        $this->getUserManipulator()->create('风中的少年', '123456', 'user2@phpdish.com', true, false);
+        $user2 = $this->getUserManipulator()->create('风中的少年', '123456', 'user2@phpdish.com', true, false);
+
+        $avatarGenerator = $this->getAvatarGenerator();
+        $user1->setAvatar($avatarGenerator->generate('优雅的风')->getKey());
+        $user2->setAvatar($avatarGenerator->generate('风中的少年')->getKey());
+
+        $userManager = $this->getUserManager();
+        $userManager->updateUser($user1);
+        $userManager->updateUser($user2);
         $this->addReference('general-user', $user1);
+    }
+
+    /**
+     * @return UserManagerInterface
+     */
+    protected function getUserManager()
+    {
+        return $this->container->get('fos_user.user_manager');
     }
 
     /**
@@ -23,5 +41,13 @@ class UserFixtures extends AbstractFixtures
     protected function getUserManipulator()
     {
         return $this->container->get('fos_user.util.user_manipulator');
+    }
+
+    /**
+     * @return AvatarGeneratorInterface
+     */
+    protected function getAvatarGenerator()
+    {
+        return $this->container->get('phpdish.avatar_generator.user');
     }
 }
