801d8d9fde08463592bf8c956dc8d1fc82188ea3
slince <taosikai@yeah.net>
2017-12-12 22:51:36 +0800

    improve

---
+++ web/build/js/default.11d17e8e42398cfef05c.js
@@ -0,0 +1,6 @@
+webpackJsonp([12],{if3c:/*!******************************!*\
+  !*** ./assets/js/default.js ***!
+  \******************************/
+/*! no static exports found */
+/*! all exports used */
+function(c,i,f){"use strict";f(/*! module/common.js */"6vhR")}},["if3c"]);
