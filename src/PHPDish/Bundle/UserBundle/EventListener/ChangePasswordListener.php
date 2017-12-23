dc1ecd872acc372eb49091072917533256005457
slince <taosikai@yeah.net>
2017-12-23 13:05:36 +0800

    修复修改密码后ui不协调的问题

---
+++ src/PHPDish/Bundle/UserBundle/EventListener/ChangePasswordListener.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace PHPDish\Bundle\UserBundle\EventListener;
+
+
+use FOS\UserBundle\Event\FormEvent;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\RouterInterface;
+
+final class ChangePasswordListener
+{
+    /**
+     * @var RouterInterface
+     */
+    protected $router;
+
+    public function __construct(RouterInterface $router)
+    {
+        $this->router = $router;
+    }
+
+    public function onChangePassword(FormEvent $event)
+    {
+        $event->setResponse(new RedirectResponse(
+            $this->router->generate('phpdish_setting_change_password') //跳转回修改密码页面
+        ));
+    }
+}
