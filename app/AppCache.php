61797811ccff3d7786cce6b31ffabda6a01513e5
slince <taosikai@sina.cn>
2018-01-10 13:31:14 +0800

    modify notice

--- app/AppCache.php
+++ app/AppCache.php
@@ -4,4 +4,19 @@ use Symfony\Bundle\FrameworkBundle\HttpCache\HttpCache;
 
 class AppCache extends HttpCache
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function getOptions()
+    {
+        return [
+            'debug' => true,
+            'default_ttl' => 0,
+            'private_headers' => ['Authorization', 'Cookie'],
+            'allow_reload' => false,
+            'allow_revalidate' => false,
+            'stale_while_revalidate' => 2,
+            'stale_if_error' => 60,
+        ];
+    }
 }
