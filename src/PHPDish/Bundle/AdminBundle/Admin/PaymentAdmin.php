344d5ff65c22687f9f2446fb9e4c0ed9968d7602
slince <taosikai@yeah.net>
2018-02-11 16:25:07 +0800

    improve

--- src/PHPDish/Bundle/AdminBundle/Admin/PaymentAdmin.php
+++ src/PHPDish/Bundle/AdminBundle/Admin/PaymentAdmin.php
@@ -8,6 +8,7 @@ use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Form\Type\Filter\ChoiceType;
 use Sonata\AdminBundle\Route\RouteCollection;
+use Sonata\AdminBundle\Show\ShowMapper;
 
 class PaymentAdmin extends AbstractAdmin
 {
@@ -22,15 +23,31 @@ class PaymentAdmin extends AbstractAdmin
     protected function configureListFields(ListMapper $list)
     {
         $list
-            ->addIdentifier('user')
+            ->addIdentifier('serialNo', null, ['label' => '流水号'])
+            ->addIdentifier('user', null, ['label' => '用户'])
             ->add('amount', 'currency', [
                 'currency' => 'CNY'
             ])
-            ->add('type')
-            ->add('status')
-            ->add('description', 'html')
-            ->add('createdAt')
-            ->add('enabled');
+            ->add('type', null, ['label' => '类型'])
+            ->add('status', null, ['label' => '状态'])
+            ->add('description', 'html', ['label' => '描述'])
+            ->add('createdAt', null, ['label' => '创建时间'])
+            ->add('enabled', null, ['label' => '是否有效']);
+    }
+
+    protected function configureShowFields(ShowMapper $show)
+    {
+        $show
+            ->add('serialNo', null, ['label' => '流水号'])
+            ->add('user', null, ['label' => '用户'])
+            ->add('amount', 'currency', [
+                'currency' => 'CNY'
+            ])
+            ->add('type', null, ['label' => '类型'])
+            ->add('status', null, ['label' => '状态'])
+            ->add('description', 'html', ['label' => '描述'])
+            ->add('createdAt', null, ['label' => '创建时间'])
+            ->add('enabled', null, ['label' => '是否有效']);
     }
 
     public function getParentAssociationMapping()
