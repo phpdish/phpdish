321ef286be58f820e9f1d510274081a360da1fb5
slince <taosikai@yeah.net>
2018-02-25 22:41:30 +0800

    fix missing migrations

---
+++ app/migrations/Version20180225144023.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types = 1);
+
+namespace PHPDish\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180225144023 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE wallets ADD freeze_amount INT NOT NULL');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE wallets DROP freeze_amount');
+    }
+}
