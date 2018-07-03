281b66f8d5dd0cdd05cc13507f2f8e0f3725e240
slince <taosikai@yeah.net>
2018-07-03 23:43:44 +0800

    add

--- web/build/js/gift.c69e58b1.js
+++
@@ -1,3 +0,0 @@
-webpackJsonp([14],{/***/
-JDLj:/***/
-function(c,n,s){"use strict";s("6vhR")}},["JDLj"]);
--- web/build/js/notification.54d4c1fe.js
+++
@@ -1,4 +0,0 @@
-webpackJsonp([13],{/***/
-MSCA:/***/
-function(n,t,e){"use strict";/* WEBPACK VAR INJECTION */
-(function(n){e("6vhR");var t=e("Jov0"),a=function(n){return n&&n.__esModule?n:{default:n}}(t);!function(){var t=n("#message_body");n("#reply-chat-form").on("submit",function(){if(0===t.val().length)return a.default.dialog.message(Translator.trans("notification.reply_content_cannot_be_empty")).flash(),!1})}()}).call(t,e("0iPh"))}},["MSCA"]);
