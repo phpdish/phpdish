705769f8e879c3335d52baf41ee127b7ad872c25
slince <taosikai@yeah.net>
2018-08-08 17:10:46 +0800

    add

--- src/PHPDish/Bundle/PostBundle/Security/PostVoter.php
+++ src/PHPDish/Bundle/PostBundle/Security/PostVoter.php
@@ -1,14 +1,42 @@
 <?php
 
+/*
+ * This file is part of the phpdish/phpdish
+ *
+ * (c) Slince <taosikai@yeah.net>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace PHPDish\Bundle\PostBundle\Security;
 
 use PHPDish\Bundle\ResourceBundle\Security\EntityVoter;
-use PHPDish\Bundle\PostBundle\Model\Post;
+use PHPDish\Bundle\PostBundle\Model\PostInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class PostVoter extends EntityVoter
 {
+    /**
+     * {@inheritdoc}
+     */
     protected function getResourceClass()
     {
-        return Post::class;
+        return PostInterface::class;
+    }
+
+    protected function canView($entity, UserInterface $user)
+    {
+        //拥有管理权限的可以阅读
+        if ($this->canEdit($entity, $user)) {
+            return true;
+        }
+        //删除的不可以阅读
+        if (!$entity->isEnabled()) {
+            return false;
+        }
+        //付费专栏检查
+        $category = $entity->getCategory();
+        return !$category->isCharging() || $category->isFollowedBy($user);
     }
 }
